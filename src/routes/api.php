d55c83664d0e7957ad10b82bd9b835b1154c6d69
Defalia <dewifarahaulia@gmail.com>
2025-05-21 19:10:38 +0700

    Update api.php untuk autentikasi

--- src/routes/api.php
+++ src/routes/api.php
@@ -16,15 +16,16 @@ Route::middleware('client.auth')->group(function (){
     Route::get('/products', [ProductApiController::class, 'index']);
     Route::post('/products', [ProductApiController::class, 'store']);
 
+    // Resourceful routes baru yang perlu autentikasi (UTS)
+    Route::apiResource('produk', ProdukApiController::class);
+    Route::apiResource('transaksi', TransaksiApiController::class);
+    Route::apiResource('detail-transaksi', DetailTransaksiApiController::class);
+    Route::apiResource('penyesuaian-stok', PenyesuaianStokApiController::class);
 });
 
 // buat test kemarin
 Route::get('/barang', [BarangApiController::class, 'index']);
 
-// Resourceful routes baru setiap API controller buat UTS
-Route::apiResource('produk', ProdukApiController::class);
+// Resourceful routes baru setiap API controller buat UTS tanpa autentikasi
 Route::apiResource('kategori', KategoriApiController::class);
 Route::apiResource('supplier', SupplierApiController::class);
-Route::apiResource('transaksi', TransaksiApiController::class);
-Route::apiResource('detail-transaksi', DetailTransaksiApiController::class);
-Route::apiResource('penyesuaian-stok', PenyesuaianStokApiController::class);

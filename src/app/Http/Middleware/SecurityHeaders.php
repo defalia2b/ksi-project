37bfbc5f0d1b2841ca4ea7f44ba8feca0d2daf1a
Defalia <dewifarahaulia@gmail.com>
2025-07-23 15:54:03 +0700

    Update security headers and session config
    agar style, font, img tetap tampil

--- src/app/Http/Middleware/SecurityHeaders.php
+++ src/app/Http/Middleware/SecurityHeaders.php
@@ -20,7 +20,7 @@ class SecurityHeaders
 
         $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
         $response->headers->set('Referrer-Policy', 'no-referrer');
-        $response->headers->set('Content-Security-Policy', "default-src 'self';");
+        $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' https://fonts.bunny.net; font-src 'self' data: https://fonts.bunny.net; img-src 'self' data: https://www.gravatar.com; connect-src 'self' ws:;");
         $response->headers->set('X-Content-Type-Options', 'nosniff');
 
         return $response;

4084e7a81e10ad363c3fd1684d1c642c9c4c3512
Defalia <dewifarahaulia@gmail.com>
2025-07-23 15:22:35 +0700

    Add security headers and enforce HTTPS configuration
    Introduced SecurityHeaders middleware and updated nginx config to add security-related headers. Changed app URL to use HTTPS and set session cookies to secure by default. These changes enhance application security by enforcing best practices for HTTP headers and secure connections.

---
+++ src/app/Http/Middleware/SecurityHeaders.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class SecurityHeaders
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
+
+        $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+        $response->headers->set('Referrer-Policy', 'no-referrer');
+        $response->headers->set('Content-Security-Policy', "default-src 'self';");
+        $response->headers->set('X-Content-Type-Options', 'nosniff');
+
+        return $response;
+    }
+} 
--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -15,7 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'client.auth' => ClientAuth::class,
+            'security.headers' => \App\Http\Middleware\SecurityHeaders::class,
         ]);
+        $middleware->append(\App\Http\Middleware\SecurityHeaders::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
